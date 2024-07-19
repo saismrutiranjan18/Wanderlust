16c374e7fa17ef70c81248f3acdf332490af1058
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-07-19 23:56:51 +0530

    third commit

--- app.js
+++ app.js
@@ -46,17 +46,18 @@ app.get("/listings/:id", async (req,res) =>{
    });
 
 //Create Route
-app.post("/listings/", async (req,res) =>{
-   const newListing = new Listing(req.body.listing);
+app.post("/listings",async (req, res) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-   });
+  }
+);
 
 //Edit Route 
 app.get("listings/:id/edit", async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("Listings/edit.ejs", {listing});
+    res.render("listings/edit.ejs", {listing});
 });
 
 //Update Route
