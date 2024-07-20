87ad9cc2a2acaf8e78ecd8a68f6e3c17e78e7aed
Jay Govind Kumar <govind.iq@gmail.com>
2024-07-20 11:04:42 +0530

    update the routes

--- app.js
+++ app.js
@@ -54,17 +54,17 @@ app.post("/listings",async (req, res) => {
 );
 
 //Edit Route 
-app.get("listings/:id/edit", async (req,res) =>{
+app.get("/listings/:id/edit", async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
 });
 
 //Update Route
-app.put("listings/:id", async (req,res) =>{
+app.put("/listings/:id", async (req,res) =>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings ${id}`);
+    res.redirect(`/listings/${id}`);
 });
 
 //Delete Route
