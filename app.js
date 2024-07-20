ee8ac97ac449581fc3a353c8d198abf3aac2523f
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-07-20 11:18:25 +0530

    fourth commit

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/listings",async (req, res) => {
 
 //Edit Route 
 app.get("listings/:id/edit", async (req,res) =>{
-    let {id} = req.params;
+    id = id.trim();
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
 });
